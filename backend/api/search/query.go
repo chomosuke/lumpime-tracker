01c5a6e322948f8c98a0941663166cbf8a63085a
chomosuke <a13323600@gmail.com>
2022-01-03 13:32:05 +1100

    query debugged

--- backend/api/search/query.go
+++ backend/api/search/query.go
@@ -12,15 +12,38 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+func deleteEmpty(s []string) []string {
+	var r []string
+	for _, str := range s {
+		if str != "" {
+			r = append(r, str)
+		}
+	}
+	return r
+}
+
 func Query(c *gin.Context) {
 	query := c.Query("query")
-	seasons := strings.Split(c.Query("seasons"), ",")
-	genres := strings.Split((c.Query("genres")), ",")
+
+	temps := deleteEmpty(strings.Split(c.Query("seasons"), ","))
+	seasons := []int{}
+	for _, temp := range temps {
+		season, err := strconv.Atoi(temp)
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+		seasons = append(seasons, season)
+	}
+
+	genres := deleteEmpty(strings.Split((c.Query("genres")), ","))
+
 	start, err := strconv.ParseInt(c.DefaultQuery("start", "0"), 10, 64)
 	if err != nil {
 		c.Status(http.StatusBadRequest)
 		return
 	}
+
 	limit, err := strconv.ParseInt(c.DefaultQuery("limit", "50"), 10, 64)
 	if err != nil {
 		c.Status(http.StatusBadRequest)
@@ -80,9 +103,13 @@ func Query(c *gin.Context) {
 			bson.D{{
 				Key: "$group",
 				Value: bson.M{
-					"_id":         bson.M{"_id": "$_id"},
-					"sortMatches": bson.M{"$sum": 1},
+					"_id":     bson.M{"_id": "$_id"},
+					"matches": bson.M{"$sum": 1},
 				},
+			}},
+			bson.D{{
+				Key:   "$project",
+				Value: bson.M{"_id": "$_id._id"},
 			}},
 		)
 	}
@@ -97,7 +124,7 @@ func Query(c *gin.Context) {
 	sortStage = append(sortStage, bson.E{Key: "seasons", Value: -1})
 
 	pipline = append(pipline, bson.D{{Key: "$sort", Value: sortStage}})
-	pipline = append(pipline, bson.D{{Key: "$project", Value: bson.M{}}})
+	pipline = append(pipline, bson.D{{Key: "$project", Value: bson.M{"id": 1}}})
 	pipline = append(pipline, bson.D{{Key: "$skip", Value: start}})
 	pipline = append(pipline, bson.D{{Key: "$limit", Value: limit}})
 
