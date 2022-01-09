d15d92144dab1adba25d47377aa014834c22da6c
chomosuke <a13323600@gmail.com>
2022-01-09 23:21:40 +1100

    removed scrollbar and top aligned not screen size grid

--- frontend/lib/views/grid.dart
+++ frontend/lib/views/grid.dart
@@ -25,27 +25,26 @@ class Grid extends HookConsumerWidget {
     }
 
     const maxCrossAxisExtent = 182 * 2.0;
-    return Scrollbar(
-      isAlwaysShown: true,
-      child: Center(
-        child: SizedBox(
-            width: 1600,
-            child: GridView.builder(
-              itemCount: filmIds.length,
-              padding: const EdgeInsets.all(30)
-                  .add(padding ?? const EdgeInsets.all(0)),
-              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: maxCrossAxisExtent,
-                childAspectRatio: 400 / 600,
-                crossAxisSpacing: 30,
-                mainAxisSpacing: 30,
-              ),
-              shrinkWrap: true,
-              itemBuilder: (context, index) => Card(
-                filmId: filmIds[index],
-                showEpisodeTracker: showEpisodeTracker,
-              ),
-            )),
+    return Container(
+      alignment: Alignment.topCenter,
+      child: SizedBox(
+        width: 1600,
+        child: GridView.builder(
+          itemCount: filmIds.length,
+          padding:
+              const EdgeInsets.all(30).add(padding ?? const EdgeInsets.all(0)),
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: maxCrossAxisExtent,
+            childAspectRatio: 400 / 600,
+            crossAxisSpacing: 30,
+            mainAxisSpacing: 30,
+          ),
+          shrinkWrap: true,
+          itemBuilder: (context, index) => Card(
+            filmId: filmIds[index],
+            showEpisodeTracker: showEpisodeTracker,
+          ),
+        ),
       ),
     );
   }
