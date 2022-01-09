8b43ecad1377f91a1bbb56825b4896155ffaeb85
chomosuke <a13323600@gmail.com>
2022-01-09 17:31:50 +1100

    shows no image if there is no image

--- frontend/lib/views/card.dart
+++ frontend/lib/views/card.dart
@@ -36,16 +36,8 @@ class Card extends HookConsumerWidget {
             child: Column(
               children: [
                 FilmDetails(filmData),
-                Image.network(
-                  filmData.imgUrl,
-                  width: 400,
-                  height: 400,
-                  filterQuality: FilterQuality.medium,
-                  fit: BoxFit.contain,
-                ),
-                FilmActions(
-                  filmId: filmId,
-                ),
+                FilmImage(imgUrl: filmData.imgUrl),
+                FilmActions(filmId: filmId),
               ],
             ),
           ),
@@ -53,6 +45,43 @@ class Card extends HookConsumerWidget {
       );
     }
     return const Center(child: CircularProgressIndicator());
+  }
+}
+
+class FilmImage extends HookConsumerWidget {
+  const FilmImage({
+    Key? key,
+    required this.imgUrl,
+  }) : super(key: key);
+
+  final String imgUrl;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return imgUrl == ''
+        ? SizedBox(
+            width: 400,
+            height: 400,
+            child: Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(),
+                ),
+                width: 200,
+                height: 200,
+                child: const Center(
+                  child: Text('No image'),
+                ),
+              ),
+            ),
+          )
+        : Image.network(
+            imgUrl,
+            width: 400,
+            height: 400,
+            filterQuality: FilterQuality.medium,
+            fit: BoxFit.contain,
+          );
   }
 }
 
