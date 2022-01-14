d64c976318e7a01556ff043fffe1914b9196c8cc
chomosuke <a13323600@gmail.com>
2022-01-15 05:42:12 +1100

    rewrote topbar with styled_widget

--- frontend/lib/views/top_bar.dart
+++ frontend/lib/views/top_bar.dart
@@ -9,6 +9,7 @@ import 'forms/index.dart';
 import 'index.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:styled_widget/styled_widget.dart';
 
 class TopBar extends HookConsumerWidget {
   final bool search;
@@ -17,53 +18,36 @@ class TopBar extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return LayoutBuilder(
-      builder: (context, constraints) => Container(
-        padding: const EdgeInsets.only(
-          left: 20,
-          top: 10,
-          right: 20,
-          bottom: 10,
+      builder: (context, constraints) => [
+        Image.network(
+          baseUrl.resolve('favicon.png').toString(),
+          height: 56,
+          filterQuality: FilterQuality.medium,
         ),
-        decoration: const BoxDecoration(
-          border: Border(
-            bottom: BorderSide(color: Colors.grey, width: 1),
+        const SizedBox(width: 20),
+        if (constraints.maxWidth > 910)
+          Text(
+            appName,
+            style: GoogleFonts.nunito(fontSize: 34, color: Colors.black54),
           ),
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.network(
-              baseUrl.resolve('favicon.png').toString(),
-              height: 56,
-              filterQuality: FilterQuality.medium,
+        const Spacer(flex: 6),
+        AnimatedCrossFade(
+          duration: const Duration(milliseconds: 300),
+          firstChild: const Search(),
+          secondChild: const SizedBox(height: 48),
+          crossFadeState:
+              search ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+        ).width(400),
+        const Spacer(flex: 1),
+        const Actions().width(150).alignment(Alignment.centerRight),
+      ]
+          .toRow(crossAxisAlignment: CrossAxisAlignment.center)
+          .padding(vertical: 10, horizontal: 20)
+          .decorated(
+            border: const Border(
+              bottom: BorderSide(color: Colors.grey, width: 1),
             ),
-            const SizedBox(width: 20),
-            if (constraints.maxWidth > 800)
-              Text(
-                appName,
-                style: GoogleFonts.nunito(fontSize: 34, color: Colors.black54),
-              ),
-            const Spacer(flex: 6),
-            SizedBox(
-              width: 400,
-              child: AnimatedCrossFade(
-                duration: const Duration(milliseconds: 300),
-                firstChild: const Search(),
-                secondChild: const SizedBox(height: 48),
-                crossFadeState: search
-                    ? CrossFadeState.showFirst
-                    : CrossFadeState.showSecond,
-              ),
-            ),
-            const Spacer(flex: 1),
-            Container(
-              alignment: Alignment.centerRight,
-              width: 150,
-              child: const Actions(),
-            )
-          ],
-        ),
-      ),
+          ),
     );
   }
 }
