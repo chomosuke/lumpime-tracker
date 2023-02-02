1e2875d53b2480fc43e2e9742f09214a439de720
chomosuke <a13323600@gmail.com>
2023-02-03 00:01:07 +1100

    provided special maskable icon and squared favicon
    also fixed local env using 80 by default

--- frontend/lib/http/url.dart
+++ frontend/lib/http/url.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'dart:io';
 
 final Uri baseUrl =
-    kReleaseMode ? Uri.base.resolve('../') : Uri.parse('http://localhost/');
+    kReleaseMode ? Uri.base.resolve('../') : Uri.parse('http://localhost:8000/');
 final Uri apiUrl = baseUrl.resolve('api/');
 
 const jsonHeader = <String, String>{
